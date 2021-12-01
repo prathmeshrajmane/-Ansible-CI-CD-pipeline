8f5d1798ddb03a4a0642679eb6011c539e1ee265
prathmeshrajmane <prathmesh.rajmane1@gmail.com>
2021-12-01 17:47:31 +0530

    updated ip script

--- ip.sh
+++ ip.sh
@@ -3,7 +3,9 @@ ip=`hostname -I | awk '{print $1}'`
 status=$(curl -o /dev/null -s -w "%(http_code)" http://$ip:60)
 if [ $status==200 ]
 then
+echo " working"
 exit 0
 else
+echo " down"
 exit 1
 fi
